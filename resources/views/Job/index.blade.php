3d1f8501a101d47a5f6ebad13825cd7b1649fb4e
unknown <asmanahasfree2@gmail.com>
2020-10-29 01:21:11 +0300

    ff

--- resources/views/Job/index.blade.php
+++ resources/views/Job/index.blade.php
@@ -1,4 +1,4 @@
-  @extends("Job\Header")
+  @extends("Job.Header")
 
 
  
@@ -372,5 +372,5 @@
      </section>  
 
      @endsection   
-      @extends("Job\Footer")  
+      @extends("Job.Footer")  
 
